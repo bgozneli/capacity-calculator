4058bcf469615fd939c73de72be8040f6cbbc54f
Ira Binta <ira.binta@jsmd-group.com>
2022-10-26 16:00:43 +0200

    adjust request body so it can be used by frontend

--- src/services/calculateCapacity.js
+++ src/services/calculateCapacity.js
@@ -1,33 +1,42 @@
 const getPublicHolidays = require("../api/publicHolidayApi");
 const db = require("../db/historical.json");
 
-const calculate = async(request) => {
-    let bspSum = 0;
-    let totalSprintCapacitySum = 0;
+const calculate = async (request) => {
+    let bspSum = request.previousSprintStats.storyPoints ?? 0;
+    let totalSprintCapacitySum = request.previousSprintStats.capacity ?? 0;
+    let dbLength = db.length;
+
+    if (request.previousSprintStats.storyPoints && request.previousSprintStats.capacity) {
+        dbLength++;
+    }
 
     const teamMembers = db[0].numberOfPeople; //historical number of people is the same
     const publicHoliday = await getPublicHolidays(new Date().getFullYear());
-    const publicHolidayCount = countIterationHolidays(request.sprintStartDate, request.noOfSprintWeeks, publicHoliday);
+    const publicHolidayCount = countIterationHolidays(
+        request.currentSprintStats.sprintStartDate,
+        request.currentSprintStats.noOfSprintWeeks,
+        publicHoliday
+    );
 
     db.forEach(function (item) {
         bspSum += item.storyPoints;
         totalSprintCapacitySum += item.capacity;
     });
 
-    const bsp = bspSum/teamMembers;
-    const totalLastSprintCapacity = totalSprintCapacitySum/teamMembers;
+    const bsp = bspSum / dbLength;
+    const totalLastSprintCapacity = totalSprintCapacitySum / dbLength;
 
-    return Math.round((((teamMembers * (14 - publicHolidayCount) - request.vacationDays)* bsp)/ totalLastSprintCapacity) * 10) / 10;
+    return Math.round((((teamMembers * (14 - publicHolidayCount) - request.currentSprintStats.vacationDays) * bsp) / totalLastSprintCapacity) * 10) / 10;
 }
 
-function countIterationHolidays(startDate, weeks, holidays){
+function countIterationHolidays(startDate, weeks, holidays) {
     const start = new Date(startDate);
     const endDate = addWeeks(weeks, start);
     let holidaysCount = 0;
 
-    holidays.forEach(function (item){
+    holidays.forEach(function (item) {
         let holidayDate = new Date(item.date);
-        if(holidayDate>=start && holidayDate<=endDate){
+        if (holidayDate >= start && holidayDate <= endDate) {
             holidaysCount++;
         }
     });
@@ -35,12 +44,23 @@ function countIterationHolidays(startDate, weeks, holidays){
     return holidaysCount;
 }
 
-function addWeeks(weekNo, start, date = new Date()){
+function addWeeks(weekNo, start, date = new Date()) {
     date.setDate(start.getDate() + weekNo * 7);
     return date;
 }
 
 // to be removed just here for testing
-calculate({vacationDays: 10, sprintStartDate: '2022-10-27', noOfSprintWeeks: 3}).then((result)=>{
+calculate({
+    currentSprintStats: {
+        vacationDays: 10,
+        sprintStartDate: '2022-10-27',
+        noOfSprintWeeks: 3
+    },
+    previousSprintStats: {
+        numberOfPeople: 5,
+        storyPoints: 15,
+        capacity: 60
+    }
+}).then((result) => {
     console.log(result);
 });
