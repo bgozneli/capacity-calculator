50e2dbec94c33ce3fb4455d813efc51d9c5e68aa
piyush biswal <piyush.biswal@jsmd-group.com>
2022-10-26 11:42:03 +0200

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReactDOM from 'react-dom/client';
 
-function App() {
+function MyForm() {
+  const [name, setName] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form>
+      <label>Enter no of Devs:
+        <input
+          type="text" 
+          value={name}
+          onChange={(e) => setNoOfDevs(e.target.value)}
+        />
+      </label>
+    </form>
+  )
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyForm />);
