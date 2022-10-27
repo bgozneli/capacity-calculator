eee7556161a1ddc903d1c30561428d420d78502a
bgoezneli <berke.goezneli@jsmd-group.com>
2022-10-27 09:30:39 +0200

    Rename Hulk to Captain Marvel.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import { useState } from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import dayjs from "dayjs";
-import * as config from './config/index';
-
+import * as config from "./config/index";
 
 function App() {
   const state = {
     currentSprintStats: {
       noOfSprintWeeks: 0,
       noOfDevs: 0,
-      sprintStartDate: '',
-      sprintEndDate: '',
+      sprintStartDate: "",
+      sprintEndDate: "",
     },
     previousSprintStats: {
       numberOfPeople: 0,
       storyPoints: 0,
       capacity: 0,
     },
-    team: 'Batman'
+    team: "Batman",
   };
-  const [noOfDevs, setNoOfDevs] = useState('0');
-  let [sprintStartDate, setSprintStartDate] = useState('');
-  let [noOfSprintWeeks, setSprintWeeks] = useState('2');
-  let [sprintEndDate, setSprintEndDate] = useState('');
-  const [lastSprintCapacity, setlastSprintCapacity] = useState('0');
-  const [lastSprintBurnDownPoints, setLastSprintBurnDownPoints] = useState('0');
-  const [lastSprintNoOfDevs, setLastSprintNoOfDevs] = useState('0');
-  const [team, setTeam] = useState('Batman');
+  const [noOfDevs, setNoOfDevs] = useState("0");
+  let [sprintStartDate, setSprintStartDate] = useState("");
+  let [noOfSprintWeeks, setSprintWeeks] = useState("2");
+  let [sprintEndDate, setSprintEndDate] = useState("");
+  const [lastSprintCapacity, setlastSprintCapacity] = useState("0");
+  const [lastSprintBurnDownPoints, setLastSprintBurnDownPoints] = useState("0");
+  const [lastSprintNoOfDevs, setLastSprintNoOfDevs] = useState("0");
+  const [team, setTeam] = useState("Batman");
   const [visible, setVisible] = useState(true);
   const [sprintVelocity, setSprintVelocity] = useState(0);
-
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -44,53 +42,78 @@ function App() {
     state.team = team;
     console.log(state);
     setSprintVelocity(15);
-  }
+  };
 
   const calculateAndSetSprintEndDate = (startDate) => {
     const weekToNoOfDayMap = config.weekToNoOfDayMap;
-    const endDate = dayjs(startDate).add(weekToNoOfDayMap[noOfSprintWeeks], 'day').format('YYYY-MM-DD');
+    const endDate = dayjs(startDate)
+      .add(weekToNoOfDayMap[noOfSprintWeeks], "day")
+      .format("YYYY-MM-DD");
     state.currentSprintStats.sprintEndDate = endDate;
     setSprintEndDate(endDate);
-  }
+  };
 
-  const checkIfHistoricalDataExist = (teamName)=> {
+  const checkIfHistoricalDataExist = (teamName) => {
     console.log(`team value ${teamName}`);
-    if(teamName === 'Joker') {
+    if (teamName === "Joker") {
       setVisible(false);
-    }else {
+    } else {
       setVisible(true);
     }
-  }
-
+  };
 
   return (
     <div className="container">
-      <h1><b>Capacity Calculator</b></h1>
+      <h1>
+        <b>Capacity Calculator</b>
+      </h1>
       <br />
       <div className="mb-3">
-          <label htmlFor="team" className="form-label" >Select Team:</label>
-          <select id="team" value={team} onChange={(e) => { setTeam(e.target.value); checkIfHistoricalDataExist(e.target.value)}} className="form-select">
-            <option value="Batman">Batman</option>
-            <option value="Joker">Joker</option>
-            <option value="Thor">Thor</option>
-            <option value="Loki">Loki</option>
-            <option value="Groot">Groot</option>
-            <option value="Hulk">Hulk</option>
-          </select>
-        </div>
-      <p><b>Current Sprint Capacity</b></p>
-      <form onSubmit={handleSubmit}  >
-
+        <label htmlFor="team" className="form-label">
+          Select Team:
+        </label>
+        <select
+          id="team"
+          value={team}
+          onChange={(e) => {
+            setTeam(e.target.value);
+            checkIfHistoricalDataExist(e.target.value);
+          }}
+          className="form-select"
+        >
+          <option value="Batman">Batman</option>
+          <option value="Joker">Joker</option>
+          <option value="Thor">Thor</option>
+          <option value="Loki">Loki</option>
+          <option value="Groot">Groot</option>
+          <option value="CaptinMarvel">CaptinMarvel</option>
+        </select>
+      </div>
+      <p>
+        <b>Current Sprint Capacity</b>
+      </p>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="sprintWeeks" className="form-label" >Sprint Weeks:</label>
-          <select id="sprintWeeks" value={noOfSprintWeeks} onChange={(e) => { setSprintWeeks(e.target.value);}} className="form-select">
+          <label htmlFor="sprintWeeks" className="form-label">
+            Sprint Weeks:
+          </label>
+          <select
+            id="sprintWeeks"
+            value={noOfSprintWeeks}
+            onChange={(e) => {
+              setSprintWeeks(e.target.value);
+            }}
+            className="form-select"
+          >
             <option value="2">2 Weeks</option>
             <option value="3">3 Weeks</option>
           </select>
         </div>
 
         <div className="mb-3">
-          <label htmlFor="DevsCount" className="form-label">Number of engineers:</label>
+          <label htmlFor="DevsCount" className="form-label">
+            Number of engineers:
+          </label>
           <input
             id="DevsCount"
             type="number"
@@ -102,20 +125,26 @@ function App() {
         </div>
 
         <div className="mb-3">
-          <label htmlFor="StartDate" className="form-label">Start Date:</label>
+          <label htmlFor="StartDate" className="form-label">
+            Start Date:
+          </label>
           <input
             className="form-control"
             id="StartDate"
             type="date"
             value={sprintStartDate}
             min="{new Date()}"
-            onChange={(e) => { setSprintStartDate(e.target.value); calculateAndSetSprintEndDate(e.target.value)}}
+            onChange={(e) => {
+              setSprintStartDate(e.target.value);
+              calculateAndSetSprintEndDate(e.target.value);
+            }}
           />
         </div>
 
-
         <div className="mb-3">
-          <label htmlFor="endDate" className="form-label">End Date:</label>
+          <label htmlFor="endDate" className="form-label">
+            End Date:
+          </label>
           <input
             id="endDate"
             className="form-control"
@@ -127,57 +156,72 @@ function App() {
 
         <br />
         <div style={{ display: visible ? "block" : "none" }}>
-        <p><b>Previous Sprint Details</b></p>
+          <p>
+            <b>Previous Sprint Details</b>
+          </p>
 
-        <div className="mb-3">
-          <label htmlFor="PreviousSprintCapacity" className="form-label">Sprint Capacity:</label>
-          <input
-            className="form-control"
-            id="PreviousSprintCapacity"
-            type="number"
-            min="0"
-            value={lastSprintCapacity}
-            onChange={(e) => setlastSprintCapacity(e.target.value)}
-          />
-        </div>
+          <div className="mb-3">
+            <label htmlFor="PreviousSprintCapacity" className="form-label">
+              Sprint Capacity:
+            </label>
+            <input
+              className="form-control"
+              id="PreviousSprintCapacity"
+              type="number"
+              min="0"
+              value={lastSprintCapacity}
+              onChange={(e) => setlastSprintCapacity(e.target.value)}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label htmlFor="PreviousSprintBurnDownPoints" className="form-label">BurnDown Points:</label>
-          <input
-            className="form-control"
-            id="PreviousSprintBurnDownPoints"
-            type="number"
-            min="0"
-            value={lastSprintBurnDownPoints}
-            onChange={(e) => setLastSprintBurnDownPoints(e.target.value)}
-          />
-        </div>
+          <div className="mb-3">
+            <label
+              htmlFor="PreviousSprintBurnDownPoints"
+              className="form-label"
+            >
+              BurnDown Points:
+            </label>
+            <input
+              className="form-control"
+              id="PreviousSprintBurnDownPoints"
+              type="number"
+              min="0"
+              value={lastSprintBurnDownPoints}
+              onChange={(e) => setLastSprintBurnDownPoints(e.target.value)}
+            />
+          </div>
 
-        <div className="mb-3">
-          <label htmlFor="PreviousSprintNoOfDevs" className="form-label">Number of engineers:</label>
-          <input
-            className="form-control"
-            id="PreviousSprintNoOfDevs"
-            type="number"
-            min="0"
-            value={lastSprintNoOfDevs}
-            onChange={(e) => setLastSprintNoOfDevs(e.target.value)}
-          />
+          <div className="mb-3">
+            <label htmlFor="PreviousSprintNoOfDevs" className="form-label">
+              Number of engineers:
+            </label>
+            <input
+              className="form-control"
+              id="PreviousSprintNoOfDevs"
+              type="number"
+              min="0"
+              value={lastSprintNoOfDevs}
+              onChange={(e) => setLastSprintNoOfDevs(e.target.value)}
+            />
+          </div>
         </div>
-        </div>
-        <input type="submit" value="Calculate" className="btn btn-primary mb-3" />
+        <input
+          type="submit"
+          value="Calculate"
+          className="btn btn-primary mb-3"
+        />
       </form>
       <div className="row">
         <div className="col-4"></div>
         <div className="col-4 align-center">
-        <label className="form-label sprint-velocity-label">Sprint Velocity</label>
-        <div className="circle">{sprintVelocity}</div>
+          <label className="form-label sprint-velocity-label">
+            Sprint Velocity
+          </label>
+          <div className="circle">{sprintVelocity}</div>
         </div>
-        
       </div>
     </div>
-  )
-
+  );
 }
 
 export default App;
